6063f326787f664cba1ab0672c0aedb13a5cbe3d
jtueros <Josue.Tueros@cignium.com>
2019-05-21 16:32:15 -0500

    first test consume

--- frontend/app/views/Entities/Reniec.jsx
+++ frontend/app/views/Entities/Reniec.jsx
@@ -1,6 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const About = () => (
+
+class Reniec extends Component {
+  render() {
+
+    return (
+      <div>
+        <div>
+          <h2>Reniec Page</h2>
+          This example shows how to use React Router!
+          <br></br>
+          DNI <input name="dni" placeholder="DNI"></input>
+          <br></br>
+          <button type="submit">Descargar</button>
+        </div>
+      </div>
+    )
+  }
+
+  constructor() {
+    super();
+    this.state = {
+      reniec: []
+    };
+  }
+  componentDidMount() {
+    setImmediate(async () => {
+      var dni = '71279117';
+      const res = await fetch(`http://localhost:3000/api/inpe/antecedentes/` + dni, { mode: "no-cors" });
+      const data = await res.json();
+
+      console.log(data);
+      console.log(data.draft_timings);
+      console.log('fin');
+      this.setState({ reniec: data });
+    });
+  }
+
+
+
+
+}
+
+/*
+const About = ({ reniec }) => (
   <div>
     <br></br>
     <br></br>
@@ -17,6 +60,7 @@ const About = () => (
   </div>
 
 );
+*/
 
+export default Reniec;
 
-export default About;
